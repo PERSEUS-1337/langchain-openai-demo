092aa2f290a2fd488604b051704e95a55d7f3bcd
Aron Resty Ramillano <abramillano@up.edu.ph>
2023-10-30 22:38:47 +0800

    feat: simplechaining with langchain

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # langchain-openai-demo
  This repository will serve as testing grounds for using OpenAI APIs inside LangChain itself, as LangChain will be the main tooling for using these different AI APIs and functions
+
+# Resources
+- [LangChain Explained in 13 Minutes | QuickStart Tutorial for Beginners - YouTube](https://www.youtube.com/watch?v=aywZrzNaKjs)
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from langchain.llms import OpenAI
 from langchain.schema import AIMessage, HumanMessage, SystemMessage
 from langchain.chat_models import ChatOpenAI
 from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain
+from langchain.chains import LLMChain, SimpleSequentialChain
 
 load_dotenv()
 api_key = os.getenv("OPENAI_API_KEY")
@@ -30,9 +30,25 @@ chat = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0.3)
 template = """You are an expert data scientist with an expertise in building deep learning models. Explain the 
 concept of {concept} in a couple of lines."""
 prompt = PromptTemplate(input_variables=["concept"], template=template)
-print(prompt)
 
-print(f"LLM: {llm(prompt.format(concept='regularization'))}")
+# print(f"LLM: {llm(prompt.format(concept='regularization'))}")
+# print(f"LLM: {llm(prompt.format(concept='autoencoder'))}")
 
 # Chains
-# chain = LLMChain(llm=llm, prompt=prompt)
+chain = LLMChain(llm=llm, prompt=prompt)
+
+# Run chain only specifying the input variable
+# print(f"Chain: {chain.run('autoencoder')}")
+
+second_prompt = PromptTemplate(
+    input_variables=["ml_concept"],
+    template="Turn the concept description of {ml_concept} and explain it to me like I'm five",
+)
+
+chain_two = LLMChain(llm=llm, prompt=second_prompt)
+
+overall_chain = SimpleSequentialChain(chains=[chain, chain_two], verbose=True)
+
+# Run chain specifying the input variable for the first chain.
+explanation = overall_chain.run("autoencoder")
+print(f"Overall Chain: {explanation}")
